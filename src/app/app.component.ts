47e94c3c42aa9678a5014e8971b44a203f6712c8
Rafael Stauffer <rafael.stauffer@hftm.ch>
2023-06-01 22:58:47 +0200

    comments for the functions

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,6 +9,7 @@ export class AppComponent {
   title = 'woodchuck calculator';
 
   formatLabel(value: number): string {
+    // for the slider label
     return `${value}`;
   }
 
@@ -16,6 +17,7 @@ export class AppComponent {
   woodchuckCalculationArray: string[] = [];
 
   onCalculate(): void {
+    // generates the text lines to output
     this.woodchuckCalculationArray = [];
     for (let i = 0; i < this.numberOfWoodchucks; i++) {
       this.woodchuckCalculationArray.push(`When woodckucks would chuck wood ${i+1} woodchuck could chuck ${i+1} wood`);
@@ -23,6 +25,7 @@ export class AppComponent {
   }
 
   getOutputStyle(iteration: number) {
+    // output gets smaller and smaller because nobody wants to read all those lines anyway
     return {
       'font-size': `${35-iteration/3}px`,
     };
