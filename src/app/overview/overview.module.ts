3d6f27c4d5ac6a2646c8f7fbaf1b07c4dbd5e3be
Rafael Stauffer <rafael.staffer@hotmail.de>
2023-06-23 13:31:06 +0200

    feat(blog-component): styling in dumb component

--- src/app/overview/overview.module.ts
+++ src/app/overview/overview.module.ts
@@ -3,9 +3,10 @@ import { CommonModule } from '@angular/common';
 
 import { OverviewRoutingModule } from './overview-routing.module';
 import { OverviewComponent } from './overview.component';
+import { BlogComponentComponent } from '../blog-component/blog-component.component';
 
 @NgModule({
-  declarations: [OverviewComponent],
+  declarations: [OverviewComponent, BlogComponentComponent],
   imports: [CommonModule, OverviewRoutingModule],
 })
 export class OverviewModule {}
